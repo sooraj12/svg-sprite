1aeccc34989562e6e9482fcbe835d011f9bbca6a
sooraj12 <soorajshaji33@gmail.com>
2023-03-16 21:31:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <svg className={`icon icon-action-menu`}>
+        <use xlinkHref="#icons_icon-action-menu" />
+      </svg>
+    </div>
+  );
+}
+
+export default App;
